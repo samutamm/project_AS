f9e0f7a995c167762a253b49fba80ff117fb84e3
samutamm <samutamm@gmail.com>
2018-11-19 16:35:37 +0100

    skeleton for SNIP pruning

--- src/Snip.py
+++ src/Snip.py
@@ -1,13 +1,30 @@
 
 import torch
+from torch import nn
+
+import numpy as np
+
+
+def weights_init_uniform(m):
+    if isinstance(m, nn.Conv2d):
+        torch.nn.init.xavier_uniform(m.weight.data)
+        #torch.nn.init.xavier_uniform(m.bias.data)
 
 class SNIP:
 
-    def __init__(self, params):
-        self.W = params
-        self.C = torch.zeros(params.shape)
+    def __init__(self, model):
+        self.model = model
+        self.C = {}
 
-    def prune(self, loss_function, training_X, training_y, K):
+        self.model.apply(weights_init_uniform) # algorithm line 1
+
+        for i, param in enumerate(self.model.parameters()):
+            if param.requires_grad:
+                self.C[i] = torch.zeros(param.shape)
+
+        self.S = self.C.copy()
+
+    def prune(self, criterion, data_loader, K):
         """
         Algorithm 1 SNIP: Single-shot Network Pruning based on Connection Sensitivity
         from the paper with corresponding name. https://openreview.net/pdf/3b4408062d47079caf01147df0e4321eb792f507.pdf
@@ -20,9 +37,28 @@ class SNIP:
         :param K: number of non-zero weights to be returned in output C
         :return: binary vector C where 1's are weights to be retained and 0's weights to drop
         """
+        X, y = next(iter(data_loader))
 
-        # TODO VarianceScalingInitialization
-        # TODO sampling examples
+        # maybe use the same optimiser than in normal learning process later
+        optimizer = torch.optim.SGD(self.model.parameters(), 0.1, momentum=0.9)
+
+        output = self.model(X)
+        loss = criterion(output, y)
+        loss.backward()
+
+        for i, param in enumerate(self.model.parameters()):
+            if param.requires_grad:
+                self.S[i] = torch.abs(param.grad) / torch.sum(torch.abs(param.grad))
+                s_values = self.S[i].numpy().flatten()
+                np.sort(s_values)
+                s_values = s_values[::-1]
+                s_k = s_values[K]
+
+                s_k_ = torch.ones(self.C[i].shape)
+                s_k_ = s_k_.new_full(self.C[i].shape, float(s_k))
+
+                self.C[i][self.S[i] > s_k_] = 1
+
         # TODO count Connection Sensivity
         # TODO sort s and take top-k
         return self.C
