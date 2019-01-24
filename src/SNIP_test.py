get gradient for fixed model

src/SNIP_test.py
@@ -3,54 +3,66 @@ from torch import nn
 from copy import deepcopy
 
 import numpy as np
-from src.cnn_mnist import *
+from cnn_mnist import *
 
-class CModule(nn.Module):
-    def __init__(self, size):
+import torch.nn.functional as F
+
+class CLinear(nn.Module):
+    def __init__(self, input_size, output_size):
         super().__init__()
-        self.c_mask = nn.Parameter(torch.ones(size))
+        w = torch.FloatTensor(output_size, input_size)
+        b = torch.FloatTensor(output_size)
+        torch.nn.init.xavier_uniform_(w)
+        #torch.nn.init.xavier_uniform_(b)
+        self.w = nn.Parameter(w)
+        self.b = nn.Parameter(b)
+        self.c_mask = nn.Parameter(torch.ones((output_size, input_size)))
 
+#        torch.nn.init.xavier_uniform_(self.b.data) no one dimensional initialisation
 
+    def forward(self, x):
+        masked_w = self.w * self.c_mask
+        activation = F.linear(x, masked_w, self.b)
+        return activation
 
 
 class Masked_model(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model_orig):
         super(Masked_model, self).__init__()
 
-        original_modules = list(model.children())
-        c_masks_mapping = {}
-        c_masks_list = []
-        i = 0
-        for c in original_modules:
-            for n, p in c.named_parameters():
-                name = 'mask_' + n.replace('.', '_')
-                c_masks_mapping[name] = i
-                c_masks_list = nn.Parameter(torch.ones(p.size()))
-                i += 1
-                # self.register_parameter(name, c_masks[n])
-
-        self.modules = nn.Sequential(* original_modules)
-        self.c_masks = nn.ModuleList(c_masks_list)
-        self.c_masks_mapping = c_masks_mapping
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 6, (5, 5), stride=1, padding=2),
+            nn.Tanh(),
+            nn.MaxPool2d((2, 2), stride=2, padding=0),
+            nn.Conv2d(6, 16, (5, 5), stride=1, padding=0),
+            nn.Tanh(),
+            nn.MaxPool2d((2, 2), stride=2, padding=0),
+        )
+        self.classifier = nn.Sequential(
+            CLinear(400, 120),
+            nn.Tanh(),
+            CLinear(120, 84),
+            nn.Tanh(),
+            CLinear(84, 10)
+            # Rappel : Le softmax est inclus dans la loss, ne pas le mettre ici
+        )
 
     def forward(self, x):
-
-        for name, param in self.modules.named_parameters():
-            mask_name = 'mask_' + name.replace('.', '_')
-            m_param = self.c_masks[self.c_masks_mapping[mask_name]]
-            param.data *= m_param.data
-
-        output = self.modules(x)
+        bsize = x.size(0)  # taille du batch
+        output = self.features(x)  # on calcule la sortie des conv
+        output = output.view(bsize, -1)  # on applati les feature map 2D en un
+        # vecteur 1D pour chaque input
+        output = self.classifier(output)  # on calcule la sortie des fc
         return output
 
 
-class SNIP(nn.Module):
+class SNIP:
     def __init__(self, model):
-        super(SNIP, self).__init__()
+        #super(SNIP, self).__init__()
         self.model = model
-        self.C_masks = nn.ParameterDict()
-        self.mask_name = []
-        self.init_C()
+        #self.C_masks = nn.ParameterDict()
+        #self.mask_name = []
+        #self.init_C()
 
     def get_total_param_number(self):
         return sum([np.prod(list(param.data.shape)) for param in self.model.parameters() if param.requires_grad])
@@ -81,49 +93,24 @@ class SNIP(nn.Module):
         """
         self.K = K
 
-        # X, y = next(iter(data_loader))
-        # print(X, y)
-        # # Register C_var
-        # for k in self.C_masks.keys():
-        #     print(self.C_masks[k])
-        # print(self.C_masks)
-        # print(list(self.C_masks.keys()))
-        # print(self.model.named_parameters())
-        #
-        # print(self.model.named_parameters()['mask_conv2_weight'])
-        state_dict = self.model.state_dict()
-        # for name, param in self.model.named_parameters():
-        #     mask_name = 'mask_' + name.replace('.', '_')
-        #     if name in self.parameters_name:
-        #         for m_name, m_param in self.model.named_parameters():
-        #             if m_name == mask_name:
-        #                 # print(m_param)
-        #                 param.data *= m_param.data
-            # print(name, param)
-        # print(self.model.state_dict())
-
-        #
-        # for i, param in enumerate(self.model.parameters()):
-        #     print(i)
-        #     param.data = nn.Parameter(param.data * self.C_masks[list(self.C_masks.keys())[i]])
-        # #
-        # # optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
-        # # optimizer.zero_grad()
+        criterion = nn.CrossEntropyLoss()
 
         self.masked_model = Masked_model(self.model)
         output = self.masked_model(X)
-        loss = F.nll_loss(output, y)
+        loss = criterion(output, y)
         print(loss.item())
         loss.backward()
         # optimizer.step()
 
-
-        mask = self.masked_model.c_masks
-        for m in mask:
-            print(mask[m].grad)
+        for name, m in self.masked_model.classifier[0].named_parameters():
+            print(name)
+            print(m.grad)
+            print(m.grad.sum())
+            #if name == 'c_mask':
+            #    import pdb; pdb.set_trace()
         # for name, param in self.model.named_parameters():
         #     if name in self.mask_name:
-        #         print(param.grad)
+        #        print(param.grad)
 
         # for k in self.C_masks.keys():
         #     print(self.C_masks[k].grad)
@@ -146,8 +133,8 @@ if __name__ == '__main__':
     epochs = 10
     torch.manual_seed(seed)
 
-    # device = torch.device("cuda" if use_cuda else "cpu")
-    device = "cpu"
+    device = torch.device("cuda" if use_cuda else "cpu")
+    #device = "cpu"
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
@@ -167,7 +154,7 @@ if __name__ == '__main__':
 
     model = Net()
     sniped_model = SNIP(model)
-    print(sniped_model.get_total_param_number())
+    '''print(sniped_model.get_total_param_number())
     # print(sniped_model.model.children())
 
     for c in sniped_model.model.children():
@@ -178,7 +165,7 @@ if __name__ == '__main__':
     for c in sniped_model.C_masks:
         print(c)
     # print(sniped_model.model.parameters()['mask_conv1_weight'])
-
+    '''
     sniped_model.compute_mask(X, y, K=10)
     # C_masks = sniped_model.C_masks
     # for i, param in enumerate(model.parameters()):
@@ -186,3 +173,4 @@ if __name__ == '__main__':
     #     param.data.copy_(param.data * C_masks[list(C_masks.keys())[i]])
 
     # C_test = deepcopy(model.state_dict())
+

src/SnipByC.py
@@ -61,11 +61,11 @@ class SNIP:
         X, y = next(iter(data_loader))
 
         # create C_var
-        C_var = torch.Tensor(1, self.get_total_param_number())
-        torch.nn.init.xavier_uniform_(C_var)
+        C_var = torch.ones(1, self.get_total_param_number())
+        #torch.nn.init.xavier_uniform_(C_var)
         C_var.requires_grad=True
 
-        optimizer = torch.optim.SGD({C_var}, lr=0.1, momentum=0.9)
+        #optimizer = torch.optim.SGD({self.model.parameters(), C_var}, lr=0.1, momentum=0.9)
 
         # Register C_var
         c_index_start = 0
@@ -88,7 +88,7 @@ class SNIP:
         output = self.model(X)
         loss = criterion(output, y)
         loss.backward()
-        optimizer.step()
+        #optimizer.step()
 
         g, gradient_mapping = self.get_all_gradients(C_var)
         self.S = np.abs(g) / np.sum(np.abs(g))