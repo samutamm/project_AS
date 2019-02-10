39449baac29c43912b713128bfddf982cf1f932f
samutamm <samutamm@gmail.com>
2019-02-10 13:47:33 +0100

    finish visualisation

--- src/visualization.py
+++ src/visualization.py
@@ -50,8 +50,8 @@ class NLayerParameterVisualizator:
                 evaluator.test_data_loader = dataloader
 
                 prune_model, snip = evaluator.create_pruning_model()
-                #_, test_losses, accuracys = evaluator.train_model(prune_model, snip)
-                #print(np.max(accuracys))
+                _, test_losses, accuracys = evaluator.train_model(prune_model, snip)
+                print(np.max(accuracys))
 
                 with torch.no_grad():
                     weights = prune_model.fc1.weight.detach().cpu().numpy()
@@ -68,14 +68,41 @@ class NLayerParameterVisualizator:
         columns = len(self.dataloaders)
         rows = 9
 
-        for i in range(columns):
+        for i in range(1,columns+1):
             for j in range(1,rows+1):
-                filename = "./src/images/" + str(i) + "_" + str(j) + ".npy"
+                filename = "./src/images/" + str(i-1) + "_" + str(j) + ".npy"
                 img = np.load(filename)
-
-                image_i = i * columns + j
+                image_i = i + columns * (j-1)
                 fig.add_subplot(rows, columns, image_i)
                 plt.imshow(img)
+        plt.show()
+
+    def sample_images(self):
+        fig = plt.figure(figsize=(8, 8))
+        columns = len(self.dataloaders)
+        rows = 9
+
+        examples = 2
+
+        for i, dataloader in enumerate(self.dataloaders):
+            X, y = next(iter(dataloader))
+            indexes = np.random.choice(X.shape[0], size=rows, replace=False)
+
+            for j in range(rows + examples):
+                if j < examples:
+                    img = X[indexes[j], 0].reshape(28,28).numpy()
+                else:
+                    filename = "./src/images/" + str(i) + "_" + str(j-examples +1 ) + ".npy"
+                    img = np.load(filename)
+
+                image_i = 1 + i + columns * j
+                ax = fig.add_subplot(rows + examples, columns, image_i)
+                ax.axis("off")
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_aspect('equal')
+                plt.imshow(img)
+        plt.subplots_adjust(wspace=0, hspace=0.1)
         plt.show()
 
 
@@ -87,8 +114,11 @@ if __name__ == '__main__':
                     transforms.Normalize((0.1307,), (0.3081,))
                 ]))
 
-    viz = NLayerParameterVisualizator(dataset, list(range(2,3)))
+    #viz = NLayerParameterVisualizator(dataset, list(range(2,10)))
+    viz = NLayerParameterVisualizator(dataset, list(range(2, 8)))
 
-    viz.train()
-    viz.combine_images()
+    viz.sample_images()
+    #viz.train()
+    #viz.combine_images()
+
 
